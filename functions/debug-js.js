5fb4086a69665c75185ea5fac503143da0644967
Ryan Neal <ry.neal@gmail.com>
2018-11-09 11:34:21 -0800

    debug js function

--- functions/debug-js.js
+++ functions/debug-js.js
@@ -1,6 +1,11 @@
+
 exports.handler = function(event, context, callback) {
+    var res = "event: " + JSON.stringify(event)
+    res += "\ncontext: " + JSON.stringify(context)
+    res += "\n"
+
     callback(null, {
     statusCode: 200,
-    body: "Hello, World"
+    body: res
     });
 }

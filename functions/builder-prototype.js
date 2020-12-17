7b51bc82af41f2b1478a1c004004839f313d1995
Ryan <ry.neal@gmail.com>
2020-12-17 15:23:49 -0800

    testing

--- functions/builder-prototype.js
+++ functions/builder-prototype.js
@@ -1,10 +1,12 @@
 exports.handler = function(event, context, callback) {
     var res = {"event": event, "context": context}
 
+    var returnval = {
+      statusCode: 200,
+      body: JSON.stringify(res),
+      headers: {"X-Nf-Experiment": "builder_func"}
+    }
+    console.log(returnval)
 
-    callback(null, {
-    statusCode: 200,
-    body: JSON.stringify(res),
-    headers: {"X-Nf-Experiment": "builder_func"}
-    });
+    callback(null, returnval);
 }

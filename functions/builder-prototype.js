6b25322f24a6345af62009093305acced6d19de7
Ryan <ry.neal@gmail.com>
2020-12-17 14:45:24 -0800

    builder function prototype

---
+++ functions/builder-prototype.js
@@ -0,0 +1,15 @@
+exports.handler = function(event, context, callback) {
+    var res = {"event": event, "context": context}
+
+    var headers = {};
+    if (event.queryStringParameters["cache"]  == true) {
+      headers = {"X-Nf-Experiment": "builder_func"};
+    }
+
+
+    callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(res),
+    headers: headers
+    });
+}
